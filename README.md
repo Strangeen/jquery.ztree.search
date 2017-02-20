1dc79bf350a3af84a4f58181c3c4d0a249e03ad6
strangeen <strangeen@aliyun.com>
2017-02-20 10:58:05 +0800

    jquery.ztree.search v1.0 released

---
+++ README.md
@@ -0,0 +1,74 @@
+> ## 概述
+
+优秀的jQuery树插件zTree的功能扩展，实现对树节点名字模糊查询并展示的方法
+
+> ## Quick Start （使用简例）
+
+
+* ### 示例代码1：
+  
+  ```html
+  <script type="text/javascript" src="jquery.ztree.search.js"></script>
+  <script type="text/javascript">
+    var zTreeObj = $.fn.zTree.init($("#tree"), setting, zNodes);
+  </script>
+  
+  <input type="text" value="" id="in" />
+  <input type="button" value="SEARCH" onclick="zTreeObj.searchNodes($('#in').val())" />
+  ```
+  
+  * ##### 描述：
+  
+    - jquery.ztree.all.min.js引入&lt;script&gt;标签下方引入jquery.ztree.search.js
+    - 点击search按钮，zTree对象zTreeObj在页面上的显示状态就为按输入框中值搜索后的结果，输入''会擦出高亮展示，tree视图不改变
+  
+
+> ## 文档
+
+* ### 配置
+
+  ```javascript
+  var setting = {
+    highlightCss: {"font-weight":"bold"},
+    disHighlightCss: {"font-weight":"inherit"}, 
+    delayTime: 500
+  }
+  ```
+
+  * #### 说明：
+    
+    * highlightCss
+      
+      高亮节点css，优先于setting.view.fontCss配置，默认值{"font-weight":"bold"}
+
+    * disHighlightCss
+      
+      取消高亮节点css，setting.view.fontCss配置优先，默认值{"font-weight":"inherit"}
+      
+      如果highlightCss中设置了color，fontCss中也设置了节点初始color，那么可以不用设置disHighlightCss的color，如果fontCss没有设置color，就需要设置disHighlightCss的color，否则节点高亮后无法取消高亮
+    
+    * delayTime 
+    
+      延迟加载时间，设置方法searchNodesLazy的默认延时时间，单位毫秒
+    
+* ### 方法
+
+  ```javascript
+  zTreeObj: {
+    searchNodes(value)
+    searchNodesLazy(value[, delay])
+  }
+  ```
+
+  * #### 说明：
+    
+    * searchNodes(value)
+      
+      搜索节点名称包含value的节点，并展开到所有搜索到的节点
+
+    * searchNodesLazy(value[, delay])
+      
+      延时调用方法searchNodes(value)，参数delay默认500毫秒，也可以通过setting.view.delayTime设置，方法中的delay优先级高于setting中的delayTime
+      
+      文本框的键盘事件触发搜索建议使用该方法，否则会导致树不断收缩和展开，同时也可能发生树收缩后无法展开的问题
+      
---
+++ jquery.ztree.search.js
@@ -0,0 +1,85 @@
+/**
+ * JQuery zTree search v1.0
+ * https://juwends.com
+ * 
+ * Copyright (c) 2017 dinghui.ye
+ *
+ * Licensed same as jquery - MIT License
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+(function($) {
+	var searchModule = {
+		// fuzzy search with value
+		searchNodes: function(value) {
+			// close highlight
+			var allNodes = this.transformToArray(this.getNodes());
+			for (var i in allNodes) {
+				allNodes[i].highlight = false;
+				this.updateNode(allNodes[i]);
+			}
+			
+			if (value == '') return; // input '' wipe highlight
+
+			// shrink all nodes
+			this.expandAll(false);
+
+			// search nodes
+			var nodes = this.getNodesByParamFuzzy('name', value);
+			// highlight searched nodes and expand them
+			for (var i in nodes) {
+				nodes[i].highlight = true;
+				this.updateNode(nodes[i]);
+				// it must expand first node first, or it can't expend to specific node
+				this.expandNode(this.getNodes()[0],true); 
+				this.expandNode(nodes[i].getParentNode(),true);
+			}
+		},
+		searchNodesLazy: function(value, delay) {
+			var searchId = this.searchId;
+			// if it is still in delayed period and another search is coming, it should clear last search request
+			if (searchId) {
+				clearTimeout(searchId);
+			}
+			var delayTime = this.setting.delayTime;
+			var that = this;
+			this.searchId = setTimeout(function(){
+				that.searchNodes(value);	
+			}, delay ? delay : delayTime ? delayTime : 500); // user setting delayTime
+		},
+		getFontCss: function(treeObj) {
+			var highlightCss = treeObj.setting.highlightCss; // user setting highlightCss
+			var disHighlightCss = treeObj.setting.disHighlightCss; // user setting disHighlightCss
+			if (!highlightCss) {
+				highlightCss = {"font-weight":"bold"};
+			}
+			if (!disHighlightCss) {
+				disHighlightCss = {"font-weight":"inherit"};
+			}
+			var defaultStyle = treeObj.setting.view.fontCss;
+			if (typeof (defaultStyle) == 'function') {
+				return (
+					function (treeId, treeNode) {
+						return (treeNode.highlight) ? 
+							$.extend(true, {}, defaultStyle(treeId, treeNode), highlightCss) :  // highlightStyle
+							$.extend(true, {}, disHighlightCss, defaultStyle(treeId, treeNode)); // defaultStyle
+					});
+			}
+			return (
+				function (treeId, treeNode) {
+					return (treeNode.highlight) ? 
+						$.extend(true, {}, defaultStyle, highlightCss) :  // highlightStyle
+						$.extend(true, {}, disHighlightCss, defaultStyle); // defaultStyle
+				});
+		}
+	};
+	var init = $.fn.zTree.init;
+	$.fn.zTree.init = function(obj, zSetting, zNodes) {
+		var treeObj = init(obj, zSetting, zNodes);
+		treeObj.searchNodes = searchModule.searchNodes;
+		treeObj.searchNodesLazy = searchModule.searchNodesLazy;
+		treeObj.setting.view.fontCss = searchModule.getFontCss(treeObj);
+		return treeObj;
+	}
+})(jQuery);
+
